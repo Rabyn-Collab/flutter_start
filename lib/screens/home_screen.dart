0d6f4977ae5c59ed493962934af5e49dacf05947
rabyn <rabyn900@gmail.com>
2022-04-20 09:49:55 +0545

    changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_projects_start/models/book.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -35,6 +35,49 @@ class HomeScreen extends StatelessWidget {
             children: [
               Image.network(
                   'https://images.unsplash.com/photo-1588580000645-4562a6d2c839?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
+                SizedBox(height: 20,),
+              Container(
+                height: 220,
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: books.length,
+                    itemBuilder: (context, index){
+                    final book = books[index];
+                       return Container(
+                         width: 350,
+                         margin: EdgeInsets.only(right: 10),
+                         child: Row(
+                           children: [
+                             Image.network(book.bookImageUrl, width: 120, height: 220,fit: BoxFit.cover,),
+                            SizedBox(width: 10,),
+                             Expanded(
+                               child: Card(
+                                 child: Column(
+                                   crossAxisAlignment: CrossAxisAlignment.start,
+                                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                   children: [
+                                     Text(book.title),
+                                     Container(
+                                         width: 160,
+                                         child: Text(book.overview, maxLines: 5, overflow: TextOverflow.ellipsis,)),
+                                     Row(
+                                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                       children: [
+                                         Text(book.star),
+                                         Text(book.genre),
+                                       ],
+                                     )
+                                   ],
+                                 ),
+                               ),
+                             )
+                           ],
+                         ),
+                       );
+                    }
+                ),
+              )
             ],
           ),
         )
