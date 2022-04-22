ef7845c1b5a058443505320573222bd23b30d41e
rabyn <rabyn900@gmail.com>
2022-04-22 09:53:04 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter_projects_start/screens/home_screen.dart';
 
 
 
-
-void main (){
+void main(){
   runApp(Home());
 }
-
 
 
 class Home extends StatelessWidget {
@@ -15,55 +13,139 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-       home: HomeScreen(),
+       debugShowCheckedModeBanner: false,
+      home:  HomeScreen(),
     );
   }
 }
 
 
 
+class Sample extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+             border: Border.all(color: Colors.black)
+            ),
+            height: 500,
+            width: 500,
+            child: Wrap(
+             // mainAxisAlignment: MainAxisAlignment.end,
+             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             // crossAxisAlignment: CrossAxisAlignment.start,
+             //  crossAxisAlignment: CrossAxisAlignment.center,
+             //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                   Container(
+                     color: Colors.black,
+                     height: 50,
+                     width: 50,
+                   ),
+                   SizedBox(width: 10,),
+                   Container(
+                     color: Colors.purple,
+                     height: 50,
+                     width: 50,
+                   ),
+                    SizedBox(width: 10,),
+                   Container(
+                     color: Colors.amber,
+                     height: 50,
+                     width: 50,
+                   ),
+                Container(
+                  color: Colors.black,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 50,
+                ),
+
+                Container(
+                  color: Colors.black,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 50,
+                ),
 
 
+              ],
+            )
 
+            // Column(
+            //  // crossAxisAlignment: CrossAxisAlignment.center,
+            //  // mainAxisAlignment: MainAxisAlignment.start,
+            //  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //    Container(
+            //      color: Colors.black,
+            //      height: 50,
+            //      width: 150,
+            //    ),
+            //    SizedBox(height: 20,),
+            //    Container(
+            //      color: Colors.purple,
+            //      height: 50,
+            //      width: 150,
+            //    ),
+            //     SizedBox(height: 20,),
+            //    Container(
+            //      color: Colors.amber,
+            //      height: 50,
+            //      width: 150,
+            //    ),
+            //
+            //
+            //   ],
+            // )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          ),
+        )
+    );
+  }
+}
 
 
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,12 +7,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+      // final appBar = AppBar(
+      //   title: Text('hello'),
+      // );
+    final navTop = MediaQuery.of(context).padding.top;
+  //  final height = MediaQuery.of(context).size.height - (appBar.preferredSize.height + navTop );
+    final height = MediaQuery.of(context).size.height - (50 + navTop );
+    print(height);
     return Scaffold(
         backgroundColor: Colors.white,
         // backgroundColor: Color(0xFF8E3200),
         // backgroundColor: Color.fromRGBO(20, 12, 45, 0.5),
         // backgroundColor: Colors.pink.withOpacity(0.9),
-        appBar: PreferredSize(
+        appBar: //appBar,
+        PreferredSize(
           preferredSize: Size(double.infinity, 50),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -30,58 +38,120 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              Image.network(
-                  'https://images.unsplash.com/photo-1588580000645-4562a6d2c839?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
-                SizedBox(height: 20,),
               Container(
-                height: 220,
+                height: height * 0.25,
+                width: double.infinity,
+                child: Image.network(
+                    'https://images.unsplash.com/photo-1588580000645-4562a6d2c839?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', fit: BoxFit.cover),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                height: 210,
                 child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: books.length,
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: books.length,
                     itemBuilder: (context, index){
-                    final book = books[index];
-                       return Container(
-                         width: 350,
-                         margin: EdgeInsets.only(right: 10),
-                         child: Row(
-                           children: [
-                             Image.network(book.bookImageUrl, width: 120, height: 220,fit: BoxFit.cover,),
+                      final book = books[index];
+                      return Container(
+                        width: 350,
+                        margin: EdgeInsets.only(right: 10),
+                        child: Row(
+                          children: [
+                            Image.network(book.bookImageUrl, width: 120, height: 210,fit: BoxFit.cover,),
                             SizedBox(width: 10,),
-                             Expanded(
-                               child: Card(
-                                 child: Column(
-                                   crossAxisAlignment: CrossAxisAlignment.start,
-                                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                   children: [
-                                     Text(book.title),
-                                     Container(
-                                         width: 160,
-                                         child: Text(book.overview, maxLines: 5, overflow: TextOverflow.ellipsis,)),
-                                     Row(
-                                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                       children: [
-                                         Text(book.star),
-                                         Text(book.genre),
-                                       ],
-                                     )
-                                   ],
-                                 ),
-                               ),
-                             )
-                           ],
-                         ),
-                       );
+                            Expanded(
+                              child: Card(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(book.title),
+                                    Container(
+                                        width: 160,
+                                        child: Text(book.overview, maxLines: 5, overflow: TextOverflow.ellipsis,)),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(book.star),
+                                        Text(book.genre),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
                     }
                 ),
-              )
+              ),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Text('You may also like', style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),),
+                ),
+              ),
+
+              // Container(
+              //   color: Colors.purple,
+              //     height: 150,
+              //     width: double.infinity,
+              //     child: FittedBox(
+              //         child: Text('alskdjlsakdjlksajdlka', style: TextStyle(fontSize: 25),)))
+
+
+
+              Container(
+                height: 269,
+                child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: books.length,
+                    itemBuilder: (context, index){
+                      final book = books[index];
+                      return Container(
+                        width: 150,
+                        margin: EdgeInsets.only(right: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                                //borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(book.bookImageUrl, width: 150, height: 220,fit: BoxFit.cover,)),
+                            SizedBox(height: 10,),
+                            Text(book.title),
+                            Text(book.genre, style: TextStyle(color: Colors.blueGrey),)
+
+                          ],
+                        ),
+                      );
+                    }
+                ),
+              ),
+
+
+
+
+
+
+
+
+
+
+
+
+
             ],
           ),
         )
     );
   }
 }
-
