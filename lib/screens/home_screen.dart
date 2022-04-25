8765904b02b38e4bb0fc7368ea92d88b004f322a
rabyn <rabyn900@gmail.com>
2022-04-25 08:27:45 +0545

    changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,45 +6,58 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+    final top = MediaQuery.of(context).padding.top;
+
+    final actualHeight = h - top - 50;
     return Scaffold(
       appBar:  PreferredSize(
         preferredSize: Size(
           double.infinity,
           50
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: AppBar(
-           // backgroundColor: Colors.black.withOpacity(0.7),
-           // backgroundColor: Colors.pink,
-          // backgroundColor: Color(0xFFF2F5F9),
-           // backgroundColor: Color.fromRGBO(2, 90, 50, 0.5),
-           // leading: Icon(Icons.book),
-           // centerTitle: true,
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            title: Text('Hi John, ', style: TextStyle(color: Colors.black),),
-            actions: [
-           //   Icon(CupertinoIcons.book),
-                Icon(Icons.search, color: Colors.black, size: 30,),
-                SizedBox(width: 10,),
-                Icon(Icons.notification_important_outlined, color: Colors.black, size: 30,),
-            ],
+        child: AppBar(
+         // backgroundColor: Colors.black.withOpacity(0.7),
+          backgroundColor: Colors.pink,
+        // backgroundColor: Color(0xFFF2F5F9),
+         // backgroundColor: Color.fromRGBO(2, 90, 50, 0.5),
+         // leading: Icon(Icons.book),
+         // centerTitle: true,
+          elevation: 0,
+        //  backgroundColor: Colors.transparent,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text('Hi John, ', style: TextStyle(color: Colors.black),),
           ),
+          actions: [
+         //   Icon(CupertinoIcons.book),
+              Icon(Icons.search, color: Colors.black, size: 30,),
+              SizedBox(width: 10,),
+              Padding(
+                padding: const EdgeInsets.only(right: 10), 
+                child: Icon(Icons.notification_important_outlined, color: Colors.black, size: 30,),
+              ),
+          ],
         ),
       ),
-        body:  Container(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-            child: Column(
-              children: [
-                Container(
-                    height: 250,
-                    width: double.infinity,
-                    child: Image.network('https://images.unsplash.com/photo-1650369335952-abb47b6a81e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60', fit: BoxFit.cover,))
-              ],
-            ),
-          ),
+        body:  SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                color: Colors.pink,
+              )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                color: Colors.black,
+              )),
+              Expanded(child: Container(
+                color: Colors.blueGrey,
+              ))
+            ],
+          )
         )
     );
   }
